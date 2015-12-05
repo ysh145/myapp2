b4822b3c3036f180022601604ee1ac015d534e1c
ysh145 <ysh145@126.com>
2015-12-05 23:48:23 +0800

    a

--- src/com/mycompany/myapp2/cordovaExample.java
+++ src/com/mycompany/myapp2/cordovaExample.java
@@ -29,8 +29,8 @@ public class cordovaExample extends DroidGap
     {
         super.onCreate(savedInstanceState);
         // Set by <content src="index.html" /> in config.xml
-        super.loadUrl(Config.getStartUrl());
-        //super.loadUrl("file:///android_asset/www/index.html")
+        //super.loadUrl(Config.getStartUrl());
+        super.loadUrl("http:///www.sh419.net");
     }
 }
 
